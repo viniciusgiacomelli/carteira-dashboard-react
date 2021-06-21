e2b34d1626e936d5a4597340b756862cf34c277f
Vinicius Giacomelli <giacomelli.vinicius@gmail.com>
2021-06-21 15:41:31 -0300

    Header principal

--- src/components/MainHeader/index.tsx
+++ src/components/MainHeader/index.tsx
@@ -1,11 +1,31 @@
-import React from 'react';
+import React, {useMemo} from 'react';
 
-import { Container } from './styles';
+import emojis from '../../utils/emojis';
+
+import { 
+    Container, 
+    Profile, 
+    Welcome, 
+    UserName
+} from './styles';
 
 const MainHeader: React.FC = () => {
+
+    const emoji = useMemo(() => {
+        const indice = Math.floor(Math.random()*emojis.length);
+        return emojis[indice];
+    },[]);
+
     return (
         <Container>
-        <h1>MainHeader</h1>
+
+            <h1>Toogle</h1>
+
+            <Profile>
+                <Welcome>Olá!{emoji}</Welcome>
+                <UserName> Nome Do Usuario</UserName>
+            </Profile>
+
         </Container>
     );
 }
--- src/components/MainHeader/styles.ts
+++ src/components/MainHeader/styles.ts
@@ -4,4 +4,19 @@ export const Container = styled.div`
     grid-area: MH;
     color:${props => props.theme.colors.white};
     background-color: ${props => props.theme.colors.secondary};
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    padding: 0 10px;
+
+    border-bottom: 1px solid ${props => props.theme.colors.gray};
 `;
+
+export const Profile = styled.div`
+    color: ${props => props.theme.colors.white};
+`;
+
+export const Welcome = styled.h3``;
+
+export const UserName = styled.span``;
