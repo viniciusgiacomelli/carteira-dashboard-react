792325a17bf5e199c84c7ff8b06d491251319697
Vinicius Giacomelli <giacomelli.vinicius@gmail.com>
2021-07-10 09:31:24 -0300

    Tema(light/dark) - Salvo no localStorage

--- src/components/hooks/themes.tsx
+++ src/components/hooks/themes.tsx
@@ -29,13 +29,23 @@ interface ITheme {
 const ThemeContext = createContext <IThemeContext> ({} as IThemeContext);
 
 const ThemeProvider: React.FC = ({ children }) => {
-    const [theme, setTheme] = useState<ITheme>(dark);
+    const [theme, setTheme] = useState<ITheme>(() => {
+        const themeSaved = localStorage.getItem('@carteira:theme');
+
+        if(themeSaved){
+            return JSON.parse(themeSaved);
+        }else{
+            return dark
+        }
+    });
 
     const toggleTheme = () => {
         if(theme.title === 'dark'){
             setTheme(light);
+            localStorage.setItem('@carteira:theme',JSON.stringify(light));
         }else{
-            setTheme(dark)
+            setTheme(dark);
+            localStorage.setItem('@carteira:theme',JSON.stringify(dark));
         };
     }
 
