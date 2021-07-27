f97105980736c59c847c2af17d00befaff676d83
Vinicius Giacomelli <giacomelli.vinicius@gmail.com>
2021-07-27 16:55:46 -0300

    auth.routes alterado

--- src/routes/auth.routes.tsx
+++ src/routes/auth.routes.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import { Switch, Route } from 'react-router';
+import { Switch, Route } from 'react-router-dom';
 
 import SignIn from '../pages/SignIn';
 
 const AuthRoutes: React.FC = () => (
     <Switch>
-        <Route path="/" component ={SignIn} />
+        <Route path="/" component={SignIn} />
     </Switch>
 
 );
